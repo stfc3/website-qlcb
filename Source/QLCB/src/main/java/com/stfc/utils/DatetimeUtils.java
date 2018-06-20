bae1cc15cb647afb5a1cdb661590575f41b6e817
viettx <viettx.dev@gmail.com>
2018-06-20 14:06:09 +0700

    viettx commit code

--- Source/QLCB/src/main/java/com/stfc/utils/DatetimeUtils.java
+++ Source/QLCB/src/main/java/com/stfc/utils/DatetimeUtils.java
@@ -30,28 +30,30 @@ public class DatetimeUtils {
             throw e;
         }
     }
-	public static int getTime(Date date, int type) {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-		int value = 0;
-		switch (type) {
-		case 1:
-			value = cal.get(Calendar.DATE);
-			break;
-		case 2:
-			value = cal.get(Calendar.MONTH) + 1;
-			break;
-		case 3:
-			value = cal.get(Calendar.YEAR);
-			break;
-		default:
-			value = 0;
-			break;
-		}
-		return value;
 
-	}
-	public static void main (String[] arg) {
-		System.out.println(getTime(new Date(), 2));
-	}
+    public static int getTime(Date date, int type) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int value = 0;
+        switch (type) {
+            case 1:
+                value = cal.get(Calendar.DATE);
+                break;
+            case 2:
+                value = cal.get(Calendar.MONTH) + 1;
+                break;
+            case 3:
+                value = cal.get(Calendar.YEAR);
+                break;
+            default:
+                value = 0;
+                break;
+        }
+        return value;
+
+    }
+
+    public static void main(String[] arg) {
+        System.out.println(getTime(new Date(), 2));
+    }
 }
